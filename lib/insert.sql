0ca26261f4a74398da31c0276b505207880e8df5
Keith Filkins <kafilkins@me.com>
2021-02-02 02:45:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Mr. Chocolate", 4, Male, Black, Happy, 1);
+  VALUES ("Mr. Chocolate", 4, M, Black, Happy, 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Rowdy", 5, Female, Brown, Hyper, 1);
+  VALUES ("Rowdy", 5, F, Brown, Hyper, 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Tabitha", 6, Female, Yellow, Calm, 0);
+  VALUES ("Tabitha", 6, F, Yellow, Calm, 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Sergeant Brown", 7, Male, Brown, Grumpy, 1);
+  VALUES ("Sergeant Brown", 7, M, Brown, Grumpy, 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Melissa", 4, Female, Red, Mellow, 1);
+  VALUES ("Melissa", 4, F, Red, Mellow, 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Grinch", 4, Male, Green, Quite, 1);
+  VALUES ("Grinch", 4, M, Green, Quite, 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Wendy", 3, Female, Orange, Nice, 0);
+  VALUES ("Wendy", 3, F, Orange, Nice, 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES (NULL, 2, Male, Burgundy, Violent, 0);
+  VALUES (NULL, 2, M, Burgundy, Violent, 0);
